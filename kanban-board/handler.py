eb45b62264f5965bc8c3fe569761ef049f1e688d
painor <pi.oussama@gmail.com>
2020-10-10 15:38:34 +0100

    Initial serverless setup

---
+++ kanban-board/handler.py
@@ -0,0 +1,16 @@
+import json
+import random
+
+
+def create_new_task(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
